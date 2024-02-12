d6a4050ff0f3d98c99f8e5d714ab5c41e1f76a48
objektwerks <objektwerks@runbox.com>
2024-02-12 14:19:01 -0500

    added entity interface

--- src/commonMain/kotlin/objektwerks/Entity.kt
+++ src/commonMain/kotlin/objektwerks/Entity.kt
@@ -1,3 +1,5 @@
 package objektwerks
 
-data class Account(val license: String, val pin: String, val email: String)
+interface Entity
+
+data class Account(val license: String, val pin: String, val email: String): Entity
