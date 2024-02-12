e25b253b729206b480c078bea61ac5c78b5944c9
objektwerks <objektwerks@runbox.com>
2024-02-12 08:04:04 -0500

    added build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+group = "objektwerks"
+version = "0.0.1-SNAPSHOT"
+
+plugins {
+    kotlin("multiplatform") version "1.9.22"
+    id("com.google.devtools.ksp") version "1.9.22-1.0.17"
+}
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvm()
+    js(IR) {
+        browser()
+    }.binaries.executable()
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation("dev.fritz2:core:"1.0-RC15")
+            }
+        }
+        val jvmMain by getting {
+            dependencies {
+            }
+        }
+        val jsMain by getting {
+            dependencies {
+            }
+        }
+    }
+}
+
+dependencies {
+    add("kspCommonMainMetadata", "dev.fritz2:lenses-annotation-processor:$fritz2Version")
+}
+kotlin.sourceSets.commonMain { kotlin.srcDir("build/generated/ksp/metadata/commonMain/kotlin") }
+tasks.withType<org.jetbrains.kotlin.gradle.dsl.KotlinCompile<*>>().all {
+    if (name != "kspCommonMainKotlinMetadata") dependsOn("kspCommonMainKotlinMetadata")
+}
