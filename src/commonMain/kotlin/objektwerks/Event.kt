8bab3206d300aabae98b8ee9c3f08a7efd9ce73b
objektwerks <objektwerks@runbox.com>
2024-02-12 15:35:05 -0500

    added serialization to event

--- src/commonMain/kotlin/objektwerks/Event.kt
+++ src/commonMain/kotlin/objektwerks/Event.kt
@@ -1,7 +1,11 @@
 package objektwerks
 
+import kotlinx.serialization.Serializable
+
 sealed interface Event
 
+@Serializable
 data class Registered(val account: Account) : Event
 
+@Serializable
 data class LoggedIn(val account: Account) : Event
