1377fed98609cfb85f525443a5273de105f68422
objektwerks <objektwerks@runbox.com>
2024-02-12 15:34:18 -0500

    added serialization to command

--- src/commonMain/kotlin/objektwerks/Command.kt
+++ src/commonMain/kotlin/objektwerks/Command.kt
@@ -1,7 +1,11 @@
 package objektwerks
 
+import kotlinx.serialization.Serializable
+
 sealed interface Command
 
+@Serializable
 data class Register(val email: Email) : Command
 
+@Serializable
 data class Login(val email: Email, val pin: Pin) : Command
