3fff8220c09a64ac9d93cd2aacbe0c95e4f3ed30
objektwerks <objektwerks@runbox.com>
2024-02-12 15:09:22 -0500

    refactored entity

--- src/commonMain/kotlin/objektwerks/Entity.kt
+++ src/commonMain/kotlin/objektwerks/Entity.kt
@@ -9,7 +9,7 @@ typealias License = String
 typealias Pin = String
 typealias Email = String
 
-data class Account(override val id: Id = 0,
-                   val license: License = "",
-                   val pin: Pin = "",
-                   val email: Email = ""): Entity
+data class Account(override val id: Id,
+                   val license: License,
+                   val pin: Pin,
+                   val email: Email) : Entity
