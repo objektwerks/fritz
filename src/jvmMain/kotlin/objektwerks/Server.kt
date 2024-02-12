13c10456858d396e2d4ad5d93ed32af305d745f1
objektwerks <objektwerks@runbox.com>
2024-02-12 15:46:21 -0500

    refactored server

--- src/jvmMain/kotlin/objektwerks/Server.kt
+++ src/jvmMain/kotlin/objektwerks/Server.kt
@@ -8,19 +8,12 @@ import io.ktor.server.routing.*
 
 import java.time.Instant
 
-class Server {
-    companion object {
-        @JvmStatic fun main(args: Array<String>): Unit = Server().run()
-    }
-
-    fun run() {
-        val server = embeddedServer(Netty, port = 7979) {
-            routing {
-                get("/now") {
-                    call.respondText("Datetime: ${Instant.now()}")
-                }
+fun main() {
+    embeddedServer(Netty, port = 7979) {
+        routing {
+            get ("/now") {
+                call.respondText("Datetime: ${Instant.now()}")
             }
         }
-        server.start(wait = true)
-    }
+    }.start(wait = true)
 }
