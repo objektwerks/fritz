476c0fc3354e03e47b1f544e8e152321888e2aa8
objektwerks <objektwerks@runbox.com>
2024-02-12 16:22:21 -0500

    enhanced store

--- src/jvmMain/kotlin/objektwerks/Store.kt
+++ src/jvmMain/kotlin/objektwerks/Store.kt
@@ -1,4 +1,19 @@
 package objektwerks
 
+import org.jetbrains.exposed.sql.Table
+
+data class H2Config(val url: String,
+                    val driver: String,
+                    val user: String,
+                    val password: String)
+
+object Accounts : Table() {
+    val id = integer("id").autoIncrement()
+    val license = varchar("license", 36)
+    val pin = varchar("pin", 7)
+    val email = varchar("email", 128)
+    override val primaryKey = PrimaryKey(id, name = "pk")
+}
+
 class Store {
 }
