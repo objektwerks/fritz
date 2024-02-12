552a9f8b642afda08c05c11d21e1bc5b8fd08e00
objektwerks <objektwerks@runbox.com>
2024-02-12 15:41:23 -0500

    implemented server

--- src/jvmMain/kotlin/objektwerks/Server.kt
+++ src/jvmMain/kotlin/objektwerks/Server.kt
@@ -1,4 +1,26 @@
 package objektwerks
 
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+import java.time.Instant
+
 class Server {
+    companion object {
+        @JvmStatic fun main(args: Array<String>): Unit = Server().run()
+    }
+
+    fun run() {
+        val server = embeddedServer(Netty, port = 7979) {
+            routing {
+                get("/now") {
+                    call.respondText("Datetime: ${Instant.now()}")
+                }
+            }
+        }
+        server.start(wait = true)
+    }
 }
