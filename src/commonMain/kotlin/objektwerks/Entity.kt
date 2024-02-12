9ad95d32413a6b6eadfdf0d28832411794a001f0
objektwerks <objektwerks@runbox.com>
2024-02-12 14:27:57 -0500

    enhanced account

--- src/commonMain/kotlin/objektwerks/Entity.kt
+++ src/commonMain/kotlin/objektwerks/Entity.kt
@@ -2,4 +2,8 @@ package objektwerks
 
 interface Entity
 
-data class Account(val license: String, val pin: String, val email: String): Entity
+typealias License = String
+typealias Pin = String
+typealias Email = String
+
+data class Account(val license: License, val pin: Pin, val email: Email): Entity
