4af4d317bc70d50c79e54f2a3a1b8be9c3331a28
objektwerks <objektwerks@runbox.com>
2024-02-13 14:22:34 -0500

    refactored ksp support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.google.devtools.ksp.gradle.KspTaskMetadata
+
 group = "objektwerks"
 version = "0.0.1-SNAPSHOT"
 
@@ -53,10 +55,5 @@ kotlin {
     }
 }
 
-dependencies {
-    add("kspCommonMainMetadata", "dev.fritz2:lenses-annotation-processor:$fritz2Version")
-}
-kotlin.sourceSets.commonMain { kotlin.srcDir("build/generated/ksp/metadata/commonMain/kotlin") }
-tasks.withType<org.jetbrains.kotlin.gradle.dsl.KotlinCompile<*>>().all {
-    if (name != "kspCommonMainKotlinMetadata") dependsOn("kspCommonMainKotlinMetadata")
-}
+dependencies.kspCommonMainMetadata("dev.fritz2:lenses-annotation-processor:$fritz2Version")
+kotlin.sourceSets.commonMain { tasks.withType<KspTaskMetadata> { kotlin.srcDir(destinationDirectory) } }
