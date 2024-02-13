f22c643b223fd66a55609edeac516bc8e7429a5f
objektwerks <objektwerks@runbox.com>
2024-02-13 18:48:06 -0500

    edited pin

--- src/jvmMain/kotlin/objektwerks/Pin.kt
+++ src/jvmMain/kotlin/objektwerks/Pin.kt
@@ -1,6 +1,20 @@
 package objektwerks
 
+import java.util.Random
 
 object Pin {
-    // TODO private val specialChars = listOf("~!@#$%^&*-+=<>?/:;")
+    private val specialChars = "~!@#$%^&*-+=<>?/:;".toList()
+    private val random = Random()
+
+    private fun newSpecialChar() : Char = specialChars( random.nextInt(specialChars.size) )
+
+    fun newInstance() : String =
+        Random.from(
+            Random
+                .alphanumeric
+                .take(5)
+                .mkString
+                .prepended(newSpecialChar)
+                .appended(newSpecialChar)
+        ).mkString
 }
