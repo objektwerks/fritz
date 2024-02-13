7a00d5b6237909afca882cfd99b9d28e43b53ae3
objektwerks <objektwerks@runbox.com>
2024-02-13 11:18:10 -0500

    enhanced handler handle

--- src/jvmMain/kotlin/objektwerks/Handler.kt
+++ src/jvmMain/kotlin/objektwerks/Handler.kt
@@ -3,7 +3,7 @@ package objektwerks
 class Handler(val store: Store) {
     fun handle(command: Command): Event =
         when( command ) {
+            is Register -> Registered( store.register(command.email) )
             is Login -> LoggedIn( store.login(command.email, command.pin) )
-            is Register -> TODO()
         }
 }
