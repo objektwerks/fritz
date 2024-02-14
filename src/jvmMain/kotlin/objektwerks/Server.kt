0a067475791629e0656afbf6dce092c9233f8397
objektwerks <objektwerks@runbox.com>
2024-02-14 18:22:13 -0500

    refactored server

--- src/jvmMain/kotlin/objektwerks/Server.kt
+++ src/jvmMain/kotlin/objektwerks/Server.kt
@@ -11,23 +11,12 @@ import io.ktor.server.routing.*
 
 import java.time.Instant
 
-import kotlinx.coroutines.runBlocking
-
 class Server {
     companion object {
         @JvmStatic fun main(args: Array<String>) {
             val store = Store()
             val handler = Handler(store)
-            val instance = Server().run(7979, handler)
-            println("Server started!")
-
-            Runtime.getRuntime().addShutdownHook(object : Thread() {
-                override fun run() = runBlocking {
-                    println("Server stopping ...")
-                    instance.stop(1000, 3000)
-                    println("Server stopped!")
-                }
-            })
+            Server().run(7979, handler)
         }
     }
 
