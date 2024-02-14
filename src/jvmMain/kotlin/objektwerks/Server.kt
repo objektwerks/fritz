0c566563fc1e269310145d1ef861165a31aee1dc
objektwerks <objektwerks@runbox.com>
2024-02-14 17:06:27 -0500

    refactored server

--- src/jvmMain/kotlin/objektwerks/Server.kt
+++ src/jvmMain/kotlin/objektwerks/Server.kt
@@ -9,12 +9,22 @@ import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
+import kotlinx.coroutines.runBlocking
+
 import java.time.Instant
 
 fun main() {
     val store = Store()
     val handler = Handler(store)
     val instance = server(7979, handler)
+
+    Runtime.getRuntime().addShutdownHook(object : Thread() {
+        override fun run() = runBlocking {
+            println("Server stopping ...")
+            instance.stop(1000, 3000)
+            println("Server stopped!")
+        }
+    })
 }
 
 fun server(port: Int, handler: Handler): NettyApplicationEngine =
