e7d475f121c81511cd53861f867f957f330d4f12
objektwerks <objektwerks@runbox.com>
2024-02-15 12:38:19 -0500

    added isValid to account

--- src/commonMain/kotlin/objektwerks/Entity.kt
+++ src/commonMain/kotlin/objektwerks/Entity.kt
@@ -1,7 +1,5 @@
 package objektwerks
 
-import dev.fritz2.core.Lenses
-
 import kotlinx.serialization.Serializable
 
 sealed interface Entity {
@@ -14,8 +12,13 @@ typealias Pin = String
 typealias Email = String
 
 @Serializable
-@Lenses
 data class Account(override val id: Id,
                    val license: License,
                    val pin: Pin,
-                   val email: Email) : Entity
+                   val email: Email) : Entity {
+    fun isValid(): Boolean =
+        (id > 0) &&
+        (license.length == 36) &&
+        (pin.length == 7) &&
+        (email.contains("@"))
+}
