bf6c54e32e99fad2645fd79749ff61c1772f919c
objektwerks <objektwerks@runbox.com>
2024-02-15 08:15:43 -0500

    implementing integration test

--- src/jvmTest/kotlin/objektwerks/IntegrationTest.kt
+++ src/jvmTest/kotlin/objektwerks/IntegrationTest.kt
@@ -1,5 +1,14 @@
 package objektwerks
 
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+
+import kotlinx.coroutines.runBlocking
+
 import org.junit.Test
 
 class IntegrationTest {
@@ -7,6 +16,22 @@ class IntegrationTest {
         val store = Store()
         val handler = Handler(store)
         val server = Server().run(7676, handler)
+        val client = HttpClient {
+            install(ContentNegotiation) {
+                json()
+            }
+        }
+        val url = "http://localhost:7676/command"
 
+        val register = Register("my@email.com")
+        runBlocking {
+            val registered = client.post(url) {
+                contentType(ContentType.Application.Json)
+                setBody(register)
+            }.body<Registered>()
+            assert( registered.account.id > 0 )
+        }
+
+        server.stop()
     }
 }
