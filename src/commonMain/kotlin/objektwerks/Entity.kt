47c51b9e3fba25d7d84b3e3e53eaeb8634550266
objektwerks <objektwerks@runbox.com>
2024-02-16 16:48:12 -0500

    added pool entity

--- src/commonMain/kotlin/objektwerks/Entity.kt
+++ src/commonMain/kotlin/objektwerks/Entity.kt
@@ -16,3 +16,10 @@ data class Account(override val id: Id,
                    val license: License,
                    val pin: Pin,
                    val email: Email) : Entity
+
+@Serializable
+data class Pool(override val id: Long,
+                val license: License,
+                val name: String,
+                val volume: Int,
+                val unit: String) : Entity
