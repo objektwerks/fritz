0b6114e6be28c5bf66476b884500c43a41daf8ff
objektwerks <objektwerks@runbox.com>
2024-02-16 14:57:54 -0500

    edited client

--- src/jsMain/kotlin/objektwerks/Client.kt
+++ src/jsMain/kotlin/objektwerks/Client.kt
@@ -4,11 +4,9 @@ import dev.fritz2.core.render
 
 fun main() {
   render("#content") {
-      h1 { +"Fritz" }
-      br {}
       div {
           p {
-              +"Hello, Fritz2!"
+              +"Hello, Pool Balance!"
           }
       }
   }
