8669802e3cc5a427c8e4534f69bc9720f7f1313c
objektwerks <objektwerks@runbox.com>
2024-02-19 13:09:48 -0500

    added saved event

--- src/commonMain/kotlin/objektwerks/Event.kt
+++ src/commonMain/kotlin/objektwerks/Event.kt
@@ -12,10 +12,10 @@ data class Registered(val account: Account) : Event
 data class LoggedIn(val account: Account) : Event
 
 @Serializable
-data class PoolsListed(val pools: List<Pool>) : Event
+data class Saved(val id: Id) : Event
 
 @Serializable
-data class PoolSaved(val pool: Pool) : Event
+data class PoolsListed(val pools: List<Pool>) : Event
 
 @Serializable
 data class Fault(val error: String, val datetime: Long = Clock.System.now().epochSeconds) : Event {
