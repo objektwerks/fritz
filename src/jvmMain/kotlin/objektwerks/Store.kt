008115fd361c788e913a6d7ee9a2ac5e18c6ae69
objektwerks <objektwerks@runbox.com>
2024-02-25 15:14:45 -0500

    refactored table column names to snake case

--- src/jvmMain/kotlin/objektwerks/Store.kt
+++ src/jvmMain/kotlin/objektwerks/Store.kt
@@ -13,7 +13,7 @@ object Accounts : Table("accounts") {
     val pin: Column<Pin> = varchar("pin", 7)
     val email: Column<Email> = varchar("email", 128)
     val created: Column<EpochSeconds> = long("created")
-    override val primaryKey = PrimaryKey(id, name = "accountPk")
+    override val primaryKey = PrimaryKey(id, name = "account_pk")
 }
 
 object Pools : Table("pools") {
@@ -22,45 +22,47 @@ object Pools : Table("pools") {
     val name: Column<Name> = varchar("name", 128)
     val volume: Column<Volume> = integer("volume")
     val uom: Column<UoM> = varchar("uom", 6)
-    override val primaryKey = PrimaryKey(id, name = "poolPk")
+    override val primaryKey = PrimaryKey(id, name = "pool_pk")
 }
 
 object Cleanings : Table("cleanings") {
     val id: Column<Id> = long("id").autoIncrement()
-    val poolId: Column<Id> = long("poolId").references(Pools.id)
+    val poolId: Column<Id> = long("pool_id").references(Pools.id)
     val brush: Column<Boolean> = bool("brush")
     val net: Column<Boolean> = bool("net")
-    val skimmerBasket: Column<Boolean> = bool("skimmerBasket")
-    val pumpBasket: Column<Boolean> = bool("pumpBasket")
-    val pumpFilter: Column<Boolean> = bool("pumpFilter")
+    val skimmerBasket: Column<Boolean> = bool("skimmer_basket")
+    val pumpBasket: Column<Boolean> = bool("pump_basket")
+    val pumpFilter: Column<Boolean> = bool("pump_filter")
     val vacuum: Column<Boolean> = bool("vacuum")
     val cleaned: Column<EpochSeconds> = long("cleaned")
-    override val primaryKey = PrimaryKey(id, name = "cleaningPk")
+    override val primaryKey = PrimaryKey(id, name = "cleaning_pk")
 }
 
 object Measurements : Table("measurements") {
     val id: Column<Id> = long("id").autoIncrement()
-    val poolId: Column<Id> = long("poolId").references(Pools.id)
-    val totalChlorine: Column<TotalChlorine> = integer("totalChlorine")
-    val freeChlorine: Column<FreeChlorine> = integer("freeChlorine")
-    val combinedChlorine: Column<CombinedChlorine> = double("combinedChlorine")
+    val poolId: Column<Id> = long("pool_id").references(Pools.id)
+    val totalChlorine: Column<TotalChlorine> = integer("total_chlorine")
+    val freeChlorine: Column<FreeChlorine> = integer("free_chlorine")
+    val combinedChlorine: Column<CombinedChlorine> = double("combined_chlorine")
     val ph: Column<Ph> = double("ph")
-    val calciumHardness: Column<CalciumHardness> = integer("calciumHardness")
-    val totalAlkalinity: Column<TotalAlkalinity> = integer("totalAlkalinity")
-    val cyanuricAcid: Column<CyanuricAcid> = integer("cyanuricAcid")
-    val totalBromine: Column<TotalBromine> = integer("totalBromine")
+    val calciumHardness: Column<CalciumHardness> = integer("calcium_hardness")
+    val totalAlkalinity: Column<TotalAlkalinity> = integer("total_alkalinity")
+    val cyanuricAcid: Column<CyanuricAcid> = integer("cyanuric_acid")
+    val totalBromine: Column<TotalBromine> = integer("total_bromine")
     val salt: Column<Salt> = integer("salt")
     val temperature: Column<Temperature> = integer("temperature")
     val measured: Column<EpochSeconds> = long("measured")
+    override val primaryKey = PrimaryKey(id, name = "measurement_pk")
 }
 
 object Chemicals : Table("chemicals") {
     val id: Column<Id> = long("id").autoIncrement()
-    val poolId: Column<Id> = long("poolId").references(Pools.id)
+    val poolId: Column<Id> = long("pool_id").references(Pools.id)
     val additive: Column<Additive> = varchar("additive", 16)
     val amount: Column<Amount> = double( "amount")
     val uom: Column<UoM> = varchar("uom", 6)
     val added: Column<EpochSeconds> = long("added")
+    override val primaryKey = PrimaryKey(id, name = "chemical_pk")
 }
 
 data class StoreConfig(val url: String,
