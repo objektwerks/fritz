dd1d9cb7801512df3072c2fcdadbc2e85072d2be
objektwerks <objektwerks@runbox.com>
2024-03-02 10:10:03 -0500

    added jodd mail dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -66,6 +66,8 @@ kotlin {
                 implementation("org.postgresql:postgresql:42.7.2")
                 implementation("com.zaxxer:HikariCP:5.1.0")
 
+                implementation("org.jodd:jodd-mail:7.0.1")
+
                 implementation("ch.qos.logback:logback-classic:1.5.0")
             }
         }
