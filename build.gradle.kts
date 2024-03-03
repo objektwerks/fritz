d883d66a751217653dea59a81e7a908dd074716d
objektwerks <objektwerks@runbox.com>
2024-03-03 09:03:53 -0500

    upgraded logback classic

--- build.gradle.kts
+++ build.gradle.kts
@@ -71,7 +71,7 @@ kotlin {
 
                 implementation("org.jodd:jodd-mail:7.0.1")
 
-                implementation("ch.qos.logback:logback-classic:1.5.1")
+                implementation("ch.qos.logback:logback-classic:1.5.2")
             }
         }
         val jvmTest by getting {
