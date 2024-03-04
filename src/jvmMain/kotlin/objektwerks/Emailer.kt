960efbc9b35e7f0d7c850147ee9a7259c02c6c3c
objektwerks <objektwerks@runbox.com>
2024-03-04 09:42:40 -0500

    added emailer config

--- src/jvmMain/kotlin/objektwerks/Emailer.kt
+++ src/jvmMain/kotlin/objektwerks/Emailer.kt
@@ -1,4 +1,14 @@
 package objektwerks
 
+import com.sksamuel.hoplite.ConfigLoader
+
+data class EmailerConfig(val host: String,
+                         val address: String,
+                         val password: String) {
+    companion object {
+        fun load(resource: String): EmailerConfig = ConfigLoader().loadConfigOrThrow<EmailerConfig>(resource)
+    }
+}
+
 class Emailer() {
 }
