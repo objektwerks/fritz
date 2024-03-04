18215d06f2c6480d6c9adde645c90ec227f0fa95
objektwerks <objektwerks@runbox.com>
2024-03-04 16:14:58 -0500

    edited fault event

--- src/commonMain/kotlin/objektwerks/Event.kt
+++ src/commonMain/kotlin/objektwerks/Event.kt
@@ -38,10 +38,10 @@ data class ChemicalsListed(val chemicals: List<Chemical>) : Event
 @Serializable
 data class ChemicalAdded(val chemical: Chemical) : Event
 
-typealias Error = String
+typealias Cause = String
 
 @Serializable
-data class Fault(val error: String, val occurred: Long = Clock.System.now().epochSeconds) : Event {
+data class Fault(val cause: Cause, val occurred: Long = Clock.System.now().epochSeconds) : Event {
     companion object {
         fun build(command: String, error: Throwable): Fault = Fault("$command failed with: ${error.message ?: "unknown error."}")
         fun build(error: String, command: Command): Fault = Fault("$error: $command")
