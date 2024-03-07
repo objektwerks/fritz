771e71aa344792e429c136c8508176d2eccc7731
objektwerks <objektwerks@runbox.com>
2024-03-07 06:53:01 -0500

    added validate command and event functions to exchanger

--- src/jvmMain/kotlin/objektwerks/Exchanger.kt
+++ src/jvmMain/kotlin/objektwerks/Exchanger.kt
@@ -15,6 +15,25 @@ class Exchanger {
             is Login    -> true
         }
 
+    private fun validate(command: Command): Fault? =
+        if (!command.isValid()) {
+            val fault = Fault.build("Invalid command", command)
+            logger.error(fault.toString())
+            fault
+        } else null
+
+    private fun validate(event: Event): Fault? {
+        val eventIsValid = event.isValid()
+        if (eventIsValid && event is Fault) store.addFault(event)
+        if (!eventIsValid) {
+            val fault = Fault.build("Invalid event", event)
+            logger.error(fault.toString())
+            store.addFault(fault)
+            return fault
+        } else return null
+    }
+
+
     suspend fun exchange(command: Command): Event {
         logger.info(command.toString())
 
