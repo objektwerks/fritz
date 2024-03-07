7d18d6a3c1cd063a84d9260b6241365c820253a7
objektwerks <objektwerks@runbox.com>
2024-03-07 06:19:54 -0500

    adapted handler test to exchange

--- src/jvmTest/kotlin/objektwerks/HandlerTest.kt
+++ src/jvmTest/kotlin/objektwerks/HandlerTest.kt
@@ -13,19 +13,15 @@ class HandlerTest {
                 .inputStream.bufferedReader().readText()
                 .also { println(it) }
 
-            val store = Store( StoreConfig.load("/store.yaml") )
-            val emailer = Emailer( EmailerConfig.load("/emailer.yaml") )
-            val handler = Handler(store, emailer)
-
-            test(handler)
-            test(store)
+            val exchange = Exchange()
+            test(exchange)
         }
     }
 
-    private suspend fun test(handler: Handler) {
+    private suspend fun test(exchange: Exchange) {
         val register = Register(EmailerConfig.load("/emailer.yaml").sender)
         assert( register.isRegister() )
-        val registered = handler.handle(register) as Registered
+        val registered = exchange.exchange(register) as Registered
         assert( registered.isRegistered() )
 
         val account = registered.account
@@ -35,87 +31,80 @@ class HandlerTest {
 
         val login = Login(account.email, account.pin)
         assert( login.isLogin() )
-        val loggedIn = handler.handle(login) as LoggedIn
+        val loggedIn = exchange.exchange(login) as LoggedIn
         assert( loggedIn.isLoggedIn() )
         assert( loggedIn.account == account )
 
         val addPool = AddPool(license, Pool(license = license, name = "pool-a"))
         assert( addPool.isAddPool() )
-        val poolAdded = handler.handle(addPool) as PoolAdded
+        val poolAdded = exchange.exchange(addPool) as PoolAdded
         assert( poolAdded.isPoolAdded() )
 
         val pool = poolAdded.pool
         assert( pool.isPool() )
         val updatePool = UpdatePool(license, pool.copy(name = "pool-z"))
         assert( updatePool.isUpdatePool() )
-        val updatedPool = handler.handle(updatePool) as Updated
+        val updatedPool = exchange.exchange(updatePool) as Updated
         assert( updatedPool.isUpdated() )
 
         val listPools = ListPools(license)
         assert( listPools.isListPools() )
-        val poolsListed = handler.handle(listPools)as PoolsListed
+        val poolsListed = exchange.exchange(listPools)as PoolsListed
         assert( poolsListed.isPoolsListed() )
         assert(poolsListed.pools.isNotEmpty())
 
         val addCleaning = AddCleaning(license, Cleaning(poolId = pool.id))
         assert( addCleaning.isAddCleaning() )
-        val cleaningAdded = handler.handle(addCleaning) as CleaningAdded
+        val cleaningAdded = exchange.exchange(addCleaning) as CleaningAdded
         assert( cleaningAdded.isCleaingAdded() )
 
         val cleaning = cleaningAdded.cleaning
         assert( cleaning.isCleaning() )
         val updateCleaning = UpdateCleaning(license, cleaning.copy(vacuum = true))
         assert( updateCleaning.isUpdateCleaning() )
-        val updatedCleaning = handler.handle(updateCleaning) as Updated
+        val updatedCleaning = exchange.exchange(updateCleaning) as Updated
         assert( updatedCleaning.isUpdated() )
 
         val listCleanings = ListCleanings(license, pool.id)
         assert( listCleanings.isListCleanings() )
-        val cleaningsListed = handler.handle(listCleanings) as CleaningsListed
+        val cleaningsListed = exchange.exchange(listCleanings) as CleaningsListed
         assert( cleaningsListed.isCleaningsListed() )
         assert(cleaningsListed.cleanings.isNotEmpty())
 
         val addMeasurement = AddMeasurement(license, Measurement(poolId = pool.id))
         assert( addMeasurement.isAddMeasurement() )
-        val measurementAdded = handler.handle(addMeasurement) as MeasurementAdded
+        val measurementAdded = exchange.exchange(addMeasurement) as MeasurementAdded
         assert( measurementAdded.isMeasurementAdded() )
 
         val measurement = measurementAdded.measurement
         assert( measurement.isMeasurement() )
         val updateMeasurement = UpdateMeasurement(license, measurement.copy(ph = 7.6))
         assert( updateMeasurement.isUpdateMeasurement() )
-        val updatedMeasurement = handler.handle(updateMeasurement) as Updated
+        val updatedMeasurement = exchange.exchange(updateMeasurement) as Updated
         assert( updatedMeasurement.isUpdated() )
 
         val listMeasurements = ListMeasurements(license, pool.id)
         assert( listMeasurements.isListMeasurements() )
-        val measurementsListed = handler.handle(listMeasurements) as MeasurementsListed
+        val measurementsListed = exchange.exchange(listMeasurements) as MeasurementsListed
         assert( measurementsListed.isMeasurementsListed() )
         assert(measurementsListed.measurements.isNotEmpty())
 
         val addChemical = AddChemical(license, Chemical(poolId = pool.id))
         assert( addChemical.isAddChemical() )
-        val chemicalAdded = handler.handle(addChemical) as ChemicalAdded
+        val chemicalAdded = exchange.exchange(addChemical) as ChemicalAdded
         assert( chemicalAdded.isChemicalAdded() )
 
         val chemical = chemicalAdded.chemical
         assert( chemical.isChemical() )
         val updateChemical = UpdateChemical(license, chemical.copy(amount = 2.0))
         assert( updateChemical.isUpdateChemical() )
-        val updatedChemical = handler.handle(updateChemical) as Updated
+        val updatedChemical = exchange.exchange(updateChemical) as Updated
         assert( updatedChemical.isUpdated() )
 
         val listChemicals = ListChemicals(license, pool.id)
         assert( listChemicals.isListChemicals() )
-        val chemicalsListed = handler.handle(listChemicals) as ChemicalsListed
+        val chemicalsListed = exchange.exchange(listChemicals) as ChemicalsListed
         assert( chemicalsListed.isChemicalsListed() )
         assert(chemicalsListed.chemicals.isNotEmpty())
-    }
-
-    private fun test(store: Store) {
-        val fault = Fault("test")
-        assert( fault.isFault() )
-        assert( store.addFault(fault) > 0 )
-        assert( store.listFaults().size == 1 )
     }
 }
