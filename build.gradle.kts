6fc1cf86600911a591ec82928d1a1049e67ebcd5
objektwerks <objektwerks@runbox.com>
2024-04-01 07:07:15 -0400

    upgraded fritz2

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ java {
 
 val kotlinVersion = "2.0.0-Beta5"
 val kotlinxSerializationVersion = "1.6.3"
-val fritz2Version = "1.0-RC16"
+val fritz2Version = "1.0-RC17"
 val ktorVersion = "2.3.9"
 val exposedVersion = "0.49.0"
 val hopliteVersion = "2.7.5"
