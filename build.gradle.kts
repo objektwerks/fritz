1b5d889936282c1912f946c6640794d73b759c83
objektwerks <objektwerks@runbox.com>
2024-05-03 06:35:20 -0400

    upgraded exposed

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ val kotlinVersion = "2.0.0-RC2"
 val kotlinxSerializationVersion = "1.6.3"
 val fritz2Version = "1.0-RC17"
 val ktorVersion = "2.3.9"
-val exposedVersion = "0.49.0"
+val exposedVersion = "0.50.0"
 val hopliteVersion = "2.7.5"
 
 kotlin {
