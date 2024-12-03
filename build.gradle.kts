e64a729d1e88ed6311b320aa21fc170306cf44a9
Objektwerks <objektwerks@runbox.com>
2024-12-03 16:54:30 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,7 +50,7 @@ kotlin {
         }
         val jvmMain by getting {
             dependencies {
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0-RC")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
 
                 implementation("io.ktor:ktor-server-core:$ktorVersion")
                 implementation("io.ktor:ktor-server-cio:$ktorVersion")
