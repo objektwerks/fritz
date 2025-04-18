2a8786a3898b903100e490d2effe982e6d8c55f5
Objektwerks <objektwerks@runbox.com>
2025-04-18 12:04:18 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -65,7 +65,7 @@ kotlin {
                 implementation("com.sksamuel.hoplite:hoplite-yaml:$hopliteVersion")
                 implementation("org.yaml:snakeyaml:2.3")
 
-                implementation("com.sksamuel.aedile:aedile-core:2.0.3")
+                implementation("com.sksamuel.aedile:aedile-core:2.1.0")
 
                 implementation("org.postgresql:postgresql:42.7.5")
                 implementation("com.zaxxer:HikariCP:6.3.0")
