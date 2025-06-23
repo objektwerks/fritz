738c0f4512dd41ea197016262050c23c8d3efa75
Objektwerks <objektwerks@runbox.com>
2025-06-23 14:51:14 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,11 +5,11 @@ group = "objektwerks"
 version = "0.6-SNAPSHOT"
 
 plugins {
-    kotlin("multiplatform") version "2.2.0-RC3"
-    kotlin("plugin.serialization") version "2.2.0-RC3"
+    kotlin("multiplatform") version "2.2.0"
+    kotlin("plugin.serialization") version "2.2.0"
     id("application")
     id("com.adarshr.test-logger") version "4.0.0"
-    id("com.google.devtools.ksp") version "2.2.0-RC3-2.0.2"
+    id("com.google.devtools.ksp") version "2.2.0-2.0.2"
 }
 
 repositories {
@@ -22,7 +22,7 @@ java {
     }
 }
 
-val kotlinVersion = "2.2.0-RC3"
+val kotlinVersion = "2.2.0"
 val kotlinxSerializationVersion = "1.8.1"
 val fritz2Version = "1.0-RC20"
 val ktorVersion = "3.2.0"
