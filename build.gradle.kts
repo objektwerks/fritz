655b96902e5da45685c84a8db18bae76f1a2834a
Objektwerks <objektwerks@runbox.com>
2025-09-12 10:19:23 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ java {
 val kotlinVersion = "2.2.20"
 val kotlinxSerializationVersion = "1.9.0"
 val fritz2Version = "1.0-RC20"
-val ktorVersion = "3.2.3"
+val ktorVersion = "3.3.0"
 val exposedVersion = "1.0.0-rc-1"
 val hopliteVersion = "3.0.0.RC1"
 
