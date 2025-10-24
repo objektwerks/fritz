c4c77e95d0f38edf5790ba7ec3a342bbd7b50a36
Objektwerks <objektwerks@runbox.com>
2025-10-24 06:19:32 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -72,7 +72,7 @@ kotlin {
 
                 implementation("org.jodd:jodd-mail:7.1.0")
 
-                implementation("ch.qos.logback:logback-classic:1.5.19")
+                implementation("ch.qos.logback:logback-classic:1.5.20")
             }
         }
         val jvmTest by getting {
