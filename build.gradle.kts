0b80380db8fc604cef3616830fde7359b1be0b00
Objektwerks <objektwerks@runbox.com>
2026-03-20 18:29:36 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -61,7 +61,7 @@ kotlin {
 
                 implementation("com.sksamuel.aedile:aedile-core:3.0.2")
 
-                implementation("org.postgresql:postgresql:42.7.8")
+                implementation("org.postgresql:postgresql:42.7.9")
                 implementation("com.zaxxer:HikariCP:7.0.2")
 
                 implementation("org.jodd:jodd-mail:7.1.0")
